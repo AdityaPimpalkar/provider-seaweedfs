1241329aea6c615facf4eb73dcdf39cb6b560580
Aditya Pimpalkar <adipimpalkar@gmail.com>
2026-09-07 18:53:46 +0530

    feat: add provider scaffold
    Signed-off-by: Aditya Pimpalkar <adipimpalkar@gmail.com>

---
+++ gen.go
@@ -0,0 +1,5 @@
+// Package providerseaweedfs contains the //go:generate directive
+// for generating the Provider CR spec for the Helm chart.
+package providerseaweedfs
+
+//go:generate go tool provider-sdk generate
